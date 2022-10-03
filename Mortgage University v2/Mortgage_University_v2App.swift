7a6320d7a18f03142afae5ec73d63ea7dd2ca1ee
christostomi <mschristostomi@gmail.com>
2022-10-03 15:56:25 -0400

    Initial Commit

---
+++ Mortgage University v2/Mortgage_University_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mortgage_University_v2App.swift
+//  Mortgage University v2
+//
+//  Created by Charles Berger on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Mortgage_University_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
